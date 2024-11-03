c1b2d7d3df753d0cb7c2b3053c732e0e32e846be
Dzmitry42 <pr30dim@gmail.com>
2024-11-03 15:43:44 +0300

    last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  protected
+  def authenticate_maglev_editor(site)
+    http_basic_authenticate_or_request_with(
+      name: ENV.fetch('MAGLEV_EDITOR_USERNAME'),
+      password: ENV.fetch('MAGLEV_EDITOR_PASSWORD'),
+      )
+  end
 end
